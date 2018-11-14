65e5d31fcd150d2f63088d071734c3f7097b09e0
Farid Alijani <farid.alijani@gmail.com>
2018-11-14 15:33:04 +0200

    first commit for OKVIS

---
+++ build_project.sh
@@ -0,0 +1,11 @@
+echo "-----------------------Compiling OKVIS-----------------------"
+
+BUILD_TYPE=Release
+NUM_PROC=8
+BASEDIR="$PWD"
+rm -rf build/
+
+mkdir build
+cd build
+cmake -DROS_BUILD_TYPE=$BUILD_TYPE ..
+make -j$NUM_PROC
