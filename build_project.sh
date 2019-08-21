b57647a5e672ed1567577101fb683bd9917f6bee
xenial <farid.alijani@gmail.com>
2019-08-21 08:17:54 +0300

    sh file modified

--- build_project.sh
+++ build_project.sh
@@ -1,11 +1,12 @@
-echo "-----------------------Compiling OKVIS-----------------------"
+echo "-----------------------Removing Existing Build-----------------------"
 
 BUILD_TYPE=Release
 NUM_PROC=8
 BASEDIR="$PWD"
 rm -rf build/
 
-mkdir build
-cd build
-cmake -DROS_BUILD_TYPE=$BUILD_TYPE ..
-make -j$NUM_PROC
+#echo "-----------------------Compiling OKVIS-----------------------"
+#mkdir build
+#cd build
+#cmake -DROS_BUILD_TYPE=$BUILD_TYPE ..
+#make -j$NUM_PROC
